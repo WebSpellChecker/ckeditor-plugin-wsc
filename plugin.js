1e748a6817aba05ca672b688f8e5ae7feb539f0d
eugene <egnedko@gmail.com>
2013-11-20 18:17:18 +0200

    Add version number plugin

--- plugin.js
+++ plugin.js
@@ -25,7 +25,7 @@ CKEDITOR.plugins.add( 'wsc', {
 		editor.config.wsc_customLoaderScript = editor.config.wsc_customLoaderScript || CKEDITOR.config.wsc_customLoaderScript;
 
 		CKEDITOR.config.wsc_cmd = editor.config.wsc_cmd || CKEDITOR.config.wsc_cmd || 'spell'; // spell, thes or grammar. default tab
-		CKEDITOR.config.wsc_version='%Rev%';
+		CKEDITOR.config.wsc_version="v4.3.0-78891f1";
 	},
 	init: function( editor ) {
 		var commandName = 'checkspell';
